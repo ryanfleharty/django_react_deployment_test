82097ba7da81432706eb4172d60c22e51a6b5a2b
Ryan Fleharty <ryan.fleharty@gmail.com>
2018-08-20 13:51:51 -0600

    added whitenoise config

--- cats_api/settings.py
+++ cats_api/settings.py
@@ -125,11 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'build/static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
